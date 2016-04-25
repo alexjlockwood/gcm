987b1dc4ce9034b698395d35de1aadf999388b8f
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-04-25 12:15:30 +0900

    new endpoint for GCM.

--- sender.go
+++ sender.go
@@ -15,7 +15,8 @@ import (
 
 const (
 	// GcmSendEndpoint is the endpoint for sending messages to the GCM server.
-	GcmSendEndpoint = "https://android.googleapis.com/gcm/send"
+	//GcmSendEndpoint = "https://android.googleapis.com/gcm/send"
+	GcmSendEndpoint = "https://gcm-http.googleapis.com/gcm/send"
 	// Initial delay before first retry, without jitter.
 	backoffInitialDelay = 1000
 	// Maximum delay before a retry.
