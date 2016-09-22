8d462dd4696a65f70f56a38a07c61ab6c11db2f1
Conrad Irwin <conrad.irwin@gmail.com>
2016-09-22 13:13:06 -0700

    Add Priority

--- message.go
+++ message.go
@@ -1,5 +1,18 @@
 package gcm
 
+// Priority represents the priority at which to send the message.
+// Google only supports two values NormalPriority (the default, and
+// equivalent to APNS level 5) and HighPriority (APNS level 10)
+type Priority string
+
+// HighPrioty is used to send messages that are urgent. For example
+// when a new email arrives.
+const HighPriority = Priority("high")
+
+// NormalPriority is used to send messages in a manner that will try
+// to optimize battery life over immediate delivery.
+const NormalPriority = Priority("normal")
+
 // Message is used by the application server to send a message to
 // the GCM server. See the documentation for GCM Architectural
 // Overview for more information:
@@ -13,6 +26,7 @@ type Message struct {
 	TimeToLive            int                    `json:"time_to_live,omitempty"`
 	RestrictedPackageName string                 `json:"restricted_package_name,omitempty"`
 	DryRun                bool                   `json:"dry_run,omitempty"`
+	Priority              Priority               `json:"priority,omitempty"`
 }
 
 // NewMessage returns a new Message with the specified payload
